6221c525c6f923ebfeda15debeb245a702990622
shamilrn <shamilrn10@gmail.com>
2024-06-20 17:29:22 +0530

    5th

--- lib/artists/artistSchedules.dart
+++ lib/artists/artistSchedules.dart
@@ -6,12 +6,117 @@ class artsSchedules extends StatefulWidget {
   @override
   State<artsSchedules> createState() => _artsSchedulesState();
 }
+var colors = [
+  Colors.red.shade100,
+  Colors.blue,
+  Colors.cyan,
+  Colors.green,
+  Colors.yellow,
+];
 
 class _artsSchedulesState extends State<artsSchedules> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text("Schedule", style: TextStyle(
+                fontSize: 25, fontWeight: FontWeight.bold, color: Colors.red
+            ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: 2,
+                itemBuilder: (BuildContext context, int index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: double.infinity,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: colors[index],
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        topLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text("1.", style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                        ),
+                        ),
+                        Spacer(),
+                        CircleAvatar(
+                          backgroundColor: Colors.grey,
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("Kochi", style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              ),
+                              ),
+                              Text("Morning show", style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              fontSize: 15,
+                              ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("Calicut", style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              ),
+                              Text("Evening show", style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 15,
+                              ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
